84faebd1da76d516f6dad7b95f33b8cc44f46417
[Engel]bert Tijskens <engelbert.tijskens@uantwerpen.be>
2024-04-19 10:14:03 +0200

    Add heat.process module

--- dpvssp/heat/process/__init__.py
+++ dpvssp/heat/process/__init__.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
-
 """
 ## Python (sub)module process
 
 read process parameters from file.
 """
 
-from pathlib import Path
 import sys
 
-p = Path(__file__).parent.parent.parent.parent.parent / 'exponential_decay'
-print(p.absolute())
-sys.path.insert(0, str(p))
+from wiptools import get_workspace_dir
+
+sys.path.insert(0, str(get_workspace_dir(__file__) / 'exponential_decay'))
 
 from exponential_decay import get_fluence, get_omega0, get_Eth_omega0, \
                               get_laser_frequency, get_mark_speed
 
 
 if __name__ == '__main__':
-    get_Eth_omega0(1)
+    Eth, omega0 = get_Eth_omega0(1)
     print('-*# finished #*-')
